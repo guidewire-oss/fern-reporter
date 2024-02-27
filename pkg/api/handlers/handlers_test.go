4ac69609fec030d0240e4d458eacd92d9dac39ce
Finsen Varghese <fvarghese@guidewire.com>
2024-02-26 21:01:26 -0800

    feat: WIP added unit tests for UpdateTestRun in handlers.go

--- pkg/api/handlers/handlers_test.go
+++ pkg/api/handlers/handlers_test.go
@@ -1,6 +1,7 @@
 package handlers_test
 
 import (
+	"bytes"
 	"database/sql"
 	"database/sql/driver"
 	"encoding/json"
@@ -8,6 +9,7 @@ import (
 	"io"
 	"net/http"
 	"net/http/httptest"
+	"regexp"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
@@ -59,7 +61,7 @@ func (mt *myTime) Scan(value interface{}) error {
 }
 
 var _ = Describe("Handlers", func() {
-	Context("when GetTestRunAll handleer is invoked", func() {
+	Context("when GetTestRunAll handler is invoked", func() {
 		It("should query db to fetch all records", func() {
 
 			rows := sqlmock.NewRows([]string{"ID", "TestProjectName"}).
@@ -116,53 +118,133 @@ var _ = Describe("Handlers", func() {
 		})
 	})
 
-	/*Context("When UpdateTestRun handler is invoked", func() {
-		It("should update record from DB by id", func() {
+	Context("when UpdateTestRun handler is invoked and test run does not exist", func() {
+		It("should return 404", func() {
+			rows := sqlmock.NewRows([]string{"ID", "TestProjectName"})
+			mock.ExpectQuery(regexp.QuoteMeta("SELECT * FROM \"test_runs\" WHERE id = $1 ORDER BY \"test_runs\".\"id\" LIMIT 1")).
+				WithArgs("123").
+				WillReturnRows(rows)
 
-			// testRunRow := sqlmock.NewRows([]string{"ID", "TestProjectName"}).
-			// 	AddRow(123, "project 123", "321", myTime(time.Now()), time.Now)
+			// Bind JSON data to gin Context
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
 
-			testRunRow := sqlmock.NewRows([]string{"id", "test_project_name"}).
-				AddRow(1, "Sample Project")
+			c.Params = append(c.Params, gin.Param{Key: "id", Value: "123"})
+			handler := handlers.NewHandler(gormDb)
+			handler.UpdateTestRun(c)
 
-			// _ := sqlmock.NewRows([]string{"id", "test_run_id", "suite_name", "start_time", "end_time"}).
-			// 	AddRow(1, 1, "Sample Suite", myTime(time.Now()), myTime(time.Now()))
+			Expect(w.Code).To(Equal(http.StatusNotFound))
+		})
+	})
 
-			// _ := sqlmock.NewRows([]string{"id", "suite_id", "spec_description", "status", "message", "start_time", "end_time"}).
-			// 	AddRow(1, 1, "Sample Spec 1", "passed", "All checks passed", myTime(time.Now()), myTime(time.Now())).
-			// 	AddRow(2, 1, "Sample Spec 2", "failed", "Assertion failed", myTime(time.Now()), myTime(time.Now()))
+	Context("when UpdateTestRun handler is invoked and test run exists", func() {
+		It("should return 200 OK", func() {
 
-			// mock.ExpectQuery("SELECT (.+) FROM \"test_runs\" WHERE id = \\$1").
-			// 	WithArgs("123").
-			// 	WillReturnRows(testRunRow)
+			expectedTestRun := models.TestRun{
+				ID:              1,
+				TestProjectName: "TestProject",
+				StartTime:       time.Now(),
+				EndTime:         time.Now(),
+				SuiteRuns: []models.SuiteRun{
+					{
+						ID:        1,
+						TestRunID: 1,
+						SuiteName: "TestSuite",
+						StartTime: time.Now(),
+						EndTime:   time.Now(),
+						SpecRuns: []models.SpecRun{
+							{
+								ID:              1,
+								SuiteID:         1,
+								SpecDescription: "TestSpec",
+								Status:          "Passed",
+								Message:         "",
+								StartTime:       time.Now(),
+								EndTime:         time.Now(),
+							},
+						},
+					},
+				},
+			}
 
-			fmt.Println(testRunRow)
-
-			mock.ExpectQuery("SELECT (.+) FROM \"test_runs\" WHERE id = \\$1").WithArgs("1").WillReturnRows(testRunRow)
-			//mock.ExpectQuery("SELECT (.+) FROM \"suite_runs\" WHERE test_run_id = \\$1").WithArgs("1").WillReturnRows(suiteRunRow)
-			//mock.ExpectQuery("SELECT (.+) FROM \"spec_runs\" WHERE suite_id = \\$1").WithArgs("1").WillReturnRows(specRunRow)
+			mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "test_runs" WHERE id = $1 ORDER BY "test_runs"."id" LIMIT 1`)).
+				WithArgs("1").
+				WillReturnRows(mock.NewRows([]string{"id", "test_project_name", "test_seed"}).
+					AddRow(expectedTestRun.ID, expectedTestRun.TestProjectName, 1))
 
 			w := httptest.NewRecorder()
 			c, _ := gin.CreateTestContext(w)
 
+			// Create a new request with JSON payload
+			jsonStr := []byte(`{"id": 1, "test_project_name":"Updated Project"}`)
+			req, err := http.NewRequest("POST", "/endpoint", bytes.NewBuffer(jsonStr))
+			if err != nil {
+				fmt.Printf("%v", err)
+			}
+
+			// Set the Content-Type header to application/json
+			req.Header.Set("Content-Type", "application/json")
+
+			c.Request = req
 			c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
 			handler := handlers.NewHandler(gormDb)
 			handler.UpdateTestRun(c)
 
-			fmt.Print(w)
+			// Check the response status code
+			Expect(w.Code).To(Equal(http.StatusOK))
 
-			Expect(w.Code).To(Equal(200))
-
-			var testRun models.TestRun
-
-			if err := json.NewDecoder(w.Body).Decode(&testRun); err != nil {
-				Fail(err.Error())
-			}
-			Expect(int(testRun.ID)).To(BeNil())
-
-			Expect(testRun.TestProjectName).To(BeNil())
 		})
-	})*/
+	})
+
+	Context("when UpdateTestRun handler is invoked with bad POST payload", func() {
+		It("should return error", func() {
+
+			expectedTestRun := models.TestRun{
+				ID:              1,
+				TestProjectName: "TestProject",
+				StartTime:       time.Now(),
+				EndTime:         time.Now(),
+			}
+
+			mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "test_runs" WHERE id = $1 ORDER BY "test_runs"."id" LIMIT 1`)).
+				WithArgs("1").
+				WillReturnRows(mock.NewRows([]string{"id", "test_project_name", "test_seed", "start_time", "end_time"}).
+					AddRow(expectedTestRun.ID, expectedTestRun.TestProjectName, expectedTestRun.TestSeed, expectedTestRun.StartTime, expectedTestRun.EndTime))
+
+			w := httptest.NewRecorder()
+			c, _ := gin.CreateTestContext(w)
+
+			// Create a new request with JSON payload
+			jsonStr := []byte(`{"BAD_PAYLOAD_KEY": "BAD_VALUE"}`)
+
+			req, err := http.NewRequest("POST", "/endpoint", bytes.NewBuffer(jsonStr))
+			if err != nil {
+				fmt.Printf("%v", err)
+			}
+
+			// Set the Content-Type header to application/json
+			req.Header.Set("Content-Type", "application/json")
+
+			c.Request = req
+			c.Params = append(c.Params, gin.Param{Key: "id", Value: "1"})
+			handler := handlers.NewHandler(gormDb)
+			handler.UpdateTestRun(c)
+
+			// Create a map to represent the response
+			var responseBody models.TestRun
+			err = json.Unmarshal(w.Body.Bytes(), &responseBody)
+
+			Expect(err).ToNot(HaveOccurred())
+			Expect(w.Code).To(Equal(http.StatusOK))
+			Expect(expectedTestRun.ID).To(Equal(responseBody.ID))
+			Expect(expectedTestRun.SuiteRuns).To(Equal(responseBody.SuiteRuns))
+			Expect(expectedTestRun.TestProjectName).To(Equal(responseBody.TestProjectName))
+			Expect(expectedTestRun.StartTime).To(BeTemporally("==", responseBody.StartTime))
+			Expect(expectedTestRun.EndTime).To(BeTemporally("==", responseBody.EndTime))
+			Expect(expectedTestRun.TestSeed).To(Equal(responseBody.TestSeed))
+
+		})
+	})
 
 	Context("When DeleteTestRun handler is invoked", func() {
 		It("should delete record from DB by id", func() {
@@ -282,4 +364,106 @@ var _ = Describe("Handlers", func() {
 		})
 
 	})
+
+	//Context("When ReportTestRunById handler is invoked", func() {
+	//	It("should query DB with where clause filtering by id", func() {
+	//
+	//		expectedTestRun := models.TestRun{
+	//			ID:              123,
+	//			TestProjectName: "TestProject",
+	//			StartTime:       time.Now(),
+	//			EndTime:         time.Now(),
+	//			SuiteRuns: []models.SuiteRun{
+	//				{
+	//					ID:        1,
+	//					TestRunID: 123,
+	//					SuiteName: "TestSuite",
+	//					StartTime: time.Now(),
+	//					EndTime:   time.Now(),
+	//					SpecRuns: []models.SpecRun{
+	//						{
+	//							ID:              1,
+	//							SuiteID:         1,
+	//							SpecDescription: "TestSpec",
+	//							Status:          "Passed",
+	//							Message:         "",
+	//							StartTime:       time.Now(),
+	//							EndTime:         time.Now(),
+	//						},
+	//					},
+	//				},
+	//			},
+	//		}
+	//
+	//		// Mock database query
+	//		mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "test_runs" WHERE id = $1 ORDER BY "test_runs"."id" LIMIT 1`)).
+	//			WithArgs("123").
+	//			WillReturnRows(mock.NewRows([]string{"id", "test_project_name", "start_time", "end_time"}).
+	//				AddRow(expectedTestRun.ID, expectedTestRun.TestProjectName, expectedTestRun.StartTime, expectedTestRun.EndTime))
+	//
+	//		mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "suite_runs" WHERE "suite_runs"."test_run_id" = $1`)).
+	//			WithArgs(123).
+	//			WillReturnRows(mock.NewRows([]string{"id", "test_run_id", "suite_name", "start_time", "end_time"}).
+	//				AddRow(expectedTestRun.SuiteRuns[0].ID, expectedTestRun.ID, expectedTestRun.SuiteRuns[0].SuiteName, expectedTestRun.SuiteRuns[0].StartTime, expectedTestRun.SuiteRuns[0].EndTime))
+	//
+	//		mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "spec_runs" WHERE "spec_runs"."suite_id" = $1`)).
+	//			WithArgs(1).
+	//			WillReturnRows(mock.NewRows([]string{"id", "suite_id", "spec_description", "status", "start_time", "end_time"}).
+	//				AddRow(expectedTestRun.SuiteRuns[0].SpecRuns[0].ID, expectedTestRun.SuiteRuns[0].ID, expectedTestRun.SuiteRuns[0].SpecRuns[0].SpecDescription, expectedTestRun.SuiteRuns[0].SpecRuns[0].Status, expectedTestRun.SuiteRuns[0].SpecRuns[0].StartTime, expectedTestRun.SuiteRuns[0].SpecRuns[0].EndTime))
+	//
+	//		// Call the method
+	//		w := httptest.NewRecorder()
+	//		c, _ := gin.CreateTestContext(w)
+	//		//gin.ena
+	//		//
+	//		c.Params = append(c.Params, gin.Param{Key: "id", Value: "123"})
+	//		handler := handlers.NewHandler(gormDb)
+	//
+	//		handler.ReportTestRunById(c)
+	//
+	//		// Verify the response
+	//		//require.Equal(t, http.StatusOK, c.Writer.Status())
+	//
+	//		// Verify the HTML output if needed
+	//		// htmlOutput := c.Writer.Body.String()
+	//		// require.Contains(t, htmlOutput, "Expected HTML output")
+	//
+	//		// Check for any remaining expectations
+	//		//require.NoError(t, mock.ExpectationsWereMet())
+	//
+	//		//specRunRow := sqlmock.NewRows([]string{"id", "suite_id", "spec_description", "status", "message", "start_time", "end_time"}).
+	//		//	AddRow(1, 11, "desc1", "status", "message", myTime(time.Now()), myTime(time.Now()))
+	//		//mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "spec_runs" WHERE "spec_runs"."suite_id" IN ($1)`)).WithArgs(1).WillReturnRows(specRunRow)
+	//		//
+	//		//suiteRunRow := sqlmock.NewRows([]string{"id", "test_run_id", "suite_name", "start_time", "end_time"}).
+	//		//	AddRow(1, 123, "Sample Spec 1", myTime(time.Now()), myTime(time.Now())).
+	//		//	AddRow(2, 123, "Sample Spec 2", myTime(time.Now()), myTime(time.Now()))
+	//		//
+	//		//mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "suite_runs" WHERE "suite_runs"."test_run_id" = $1`)).WithArgs(123).WillReturnRows(suiteRunRow)
+	//		//
+	//		//rows := sqlmock.NewRows([]string{"ID", "TestProjectName"}).
+	//		//	AddRow(123, "project 1")
+	//		//
+	//		//mock.ExpectQuery(regexp.QuoteMeta(`SELECT \* FROM "spec_runs" WHERE "spec_runs"\."suite_id" IN \(\$1\)`)).
+	//		//	WithArgs("123").
+	//		//	WillReturnRows(rows)
+	//		//
+	//		//w := httptest.NewRecorder()
+	//		//c, _ := gin.CreateTestContext(w)
+	//		//
+	//		//c.Params = append(c.Params, gin.Param{Key: "id", Value: "123"})
+	//		//handler := handlers.NewHandler(gormDb)
+	//		//handler.ReportTestRunById(c)
+	//		//
+	//		//Expect(w.Code).To(Equal(200))
+	//		//
+	//		//var testRun models.TestRun
+	//		//
+	//		//if err := json.NewDecoder(w.Body).Decode(&testRun); err != nil {
+	//		//	Fail(err.Error())
+	//		//}
+	//		//Expect(int(testRun.ID)).To(Equal(123))
+	//		//Expect(testRun.TestProjectName).To(Equal("project 123"))
+	//	})
+	//})
 })
