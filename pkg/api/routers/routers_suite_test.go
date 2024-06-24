6aab37838644ea167531c251013bfb844412d5e2
Marius Williams <marwilliams@guidewire.com>
2024-06-24 00:54:25 -0500

    feat: Loading config for routers test.

--- pkg/api/routers/routers_suite_test.go
+++ pkg/api/routers/routers_suite_test.go
@@ -1,6 +1,7 @@
 package routers_test
 
 import (
+	"github.com/guidewire/fern-reporter/config"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -11,3 +12,8 @@ func TestRouters(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Routers Suite")
 }
+
+var _ = BeforeSuite(func() {
+	_, err := config.LoadConfig()
+	Expect(err).NotTo(HaveOccurred())
+})
