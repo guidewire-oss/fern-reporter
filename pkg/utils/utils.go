e61843e35470dff232e2ab4813f989b2d150ab2c
Swathi Loganathan <swathijubilant@gmail.com>
2024-07-28 17:35:37 -0700

    Fix: Update embedded TestRuns HTML report to calculate and display test run metrics
    Signed-off-by: Swathi Loganathan <swathijubilant@gmail.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,8 +1,16 @@
 package utils
 
-import "time"
+import (
+	"github.com/guidewire/fern-reporter/pkg/models"
+	"time"
+)
 
-const DateLayoutFormat = "2006-01-02 15:04:05"
+const (
+	DateLayoutFormat = "2006-01-02 15:04:05"
+	StatusSkipped    = "skipped"
+	StatusPassed     = "passed"
+	StatusFailed     = "failed"
+)
 
 func CalculateDuration(start, end time.Time) string {
 	duration := end.Sub(start)
@@ -11,4 +19,24 @@ func CalculateDuration(start, end time.Time) string {
 
 func FormatDate(t time.Time) string {
 	return t.Format(DateLayoutFormat)
+}
+
+// Common function to calculate test metrics
+func CalculateTestMetrics(testRuns []models.TestRun) (totalTests, executedTests, passedTests, failedTests int) {
+	for _, testRun := range testRuns {
+		for _, suiteRun := range testRun.SuiteRuns {
+			for _, specRun := range suiteRun.SpecRuns {
+				totalTests++ // Count each spec run
+				if specRun.Status != StatusSkipped {
+					executedTests++ // Count only executed spec runs
+					if specRun.Status == StatusPassed {
+						passedTests++ // Count passed spec runs
+					} else if specRun.Status == StatusFailed {
+						failedTests++ // Count failed spec runs
+					}
+				}
+			}
+		}
+	}
+	return
 }
