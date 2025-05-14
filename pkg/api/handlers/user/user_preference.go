77abd23706a0c0988e8b17fb895215fc6f31adf3
Gagan H R <92567822+gaganhr94@users.noreply.github.com>
2025-05-14 07:05:49 +0530

    feat: addition of the get favorites endpoint (#263)

--- pkg/api/handlers/user/user_preference.go
+++ pkg/api/handlers/user/user_preference.go
@@ -141,6 +141,30 @@ func (h *UserHandler) DeleteFavouriteProject(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": fmt.Sprintf("Favourite Project %s deleted successfully", project.UUID)})
 }
 
+func (h *UserHandler) GetFavouriteProject(c *gin.Context) {
+    ucookie, _ := c.Cookie(utils.CookieName)
+
+    user, err := GetUserObject(h, ucookie)
+    if err != nil {
+        c.JSON(http.StatusNotFound, gin.H{"error": fmt.Sprintf("User ID not found: %v", err)})
+        return
+    }
+
+    var uuids []string
+    err = h.db.
+        Table("preferred_projects").
+        Joins("JOIN project_details ON preferred_projects.project_id = project_details.id").
+        Where("preferred_projects.user_id = ? AND preferred_projects.group_id IS NULL", user.ID).
+        Pluck("project_details.uuid", &uuids).Error
+
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "error fetching favourite project uuids"})
+        return
+    }
+
+    c.JSON(http.StatusOK, uuids)
+}
+
 func (h *UserHandler) SaveUserPreference(c *gin.Context) {
 	var preference UserPreferenceRequest
 	ucookie, _ := c.Cookie(utils.CookieName)
